0c22868015421116add85fb83bb6c736931c8584
Emerson Pereira <emerson.pereira@compasso.com.br>
2020-04-15 19:36:35 -0300

    Fix: Likes

--- src/app.js
+++ src/app.js
@@ -40,10 +40,11 @@ app.delete("/repositories/:id", (request, response) => {
 
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
-  const repositorieIndex =  repositories.findIndex(repositorie => repositorie.id === id);
-  if(repositorieIndex < 0 ) return response.status(400).json({error: 'Repositorie not found.'})
-  repositories[repositorieIndex].likes++  ;
-  return response.status(200).json(repositories[repositorieIndex]);
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+  if ( repositoryIndex < 0 ) return response.status(400).json({ error: 'Repository not found!' });
+  const likes = repositories[repositoryIndex].likes + 1;
+  repositories[repositoryIndex] = { ...repositories[repositoryIndex], likes, };
+  return response.json({ likes });
 });
 
 module.exports = app;
